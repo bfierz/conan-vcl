d1d7354fb23bed5db0a28c0e453b7072b6990d46
Basil Fierz <basil.fierz@hotmail.com>
2019-05-10 20:36:50 +0200

    Import VCL conan configuration

--- conanfile.py
+++ conanfile.py
@@ -1,73 +1,144 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
+import shutil
 import os
 
-
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    exports = ["LICENSE.md"]      # Packages the license for the conanfile.py
-    # Remove following lines if the target lib does not use cmake.
+    name = "vcl"
+    version = "20190502"
+    commit_id = "7b73e9914fc15165bbc3d61e70fc0de804e440a7"
+    description = "Visual Computing Library (VCL)"
+    topics = ("Visual Computing")
+    url = "https://github.com/bfierz/vcl"
+    homepage = "https://github.com/bfierz/vcl"
+    author = "Basil Fierz <basil.fierz@hotmail.com>"
+    license = "MIT"
+    exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
 
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = { 
+            "vectorization": ["AVX", "AVX 2", "SSE 4.2" ], 
+            "fPIC": [True, False]
+            }
+    default_options = {
+            "vectorization": "AVX",
+            "fPIC": True
+    }
 
     # Custom attributes for Bincrafters recipe conventions
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
     requires = (
-        "OpenSSL/1.0.2r@conan/stable",
-        "zlib/1.2.11@conan/stable"
+        "abseil/20180600@bincrafters/stable",
+        "eigen/3.3.7@conan/stable",
+        "fmt/4.1.0@bincrafters/stable",
+        "glew/2.1.0@bincrafters/stable"
     )
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
+        else:
+            self.options["abseil"].fPIC = self.options.fPIC
+            self.options["glew"].fPIC = self.options.fPIC
+
+        # Support multi-package configuration
+        #if self.settings.compiler == "Visual Studio":
+        #    del self.settings.build_type
+        #    del self.settings.compiler["Visual Studio"].runtime
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version), sha256="Please-provide-a-checksum")
-        extracted_dir = self.name + "-" + self.version
+        
+        self.run("git clone --recursive " + self.url + ".git " + self._source_subfolder)
+        shutil.rmtree(self._source_subfolder + "/src/externals/json/test")
 
-        # Rename to "source_subfolder" is a convention to simplify later steps
-        os.rename(extracted_dir, self._source_subfolder)
-
+        #source_url = "https://github.com/bfierz/vcl"
+        #tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.commit_id))
+        #extracted_dir = self.name + "-" + self.commit_id
+        #os.rename(extracted_dir, self._source_subfolder)
+        
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
+
+        # Configure which parts to build
+        cmake.definitions["VCL_BUILD_BENCHMARKS"] = False
+        cmake.definitions["VCL_BUILD_TESTS"] = False
+        cmake.definitions["VCL_BUILD_TOOLS"] = False
+        cmake.definitions["VCL_BUILD_EXAMPLES"] = False
+        # Support multi-package configuration
+        #if not hasattr(self.settings, "build_type"):
+        #    cmake.definitions["CMAKE_DEBUG_POSTFIX"] = "_d"
+        # Required for compatibility atm
+        cmake.definitions["CMAKE_DEBUG_POSTFIX"] = "_d"
+        # Configure features
+        cmake.definitions["VCL_VECTORIZE"] = str(self.options.vectorization)
+        cmake.definitions["VCL_OPENGL_SUPPORT"] = True
+        if self.settings.os != "Windows":
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        # Configure external targets
+        cmake.definitions["vcl_ext_absl"] = "CONAN_PKG::abseil"
+        cmake.definitions["vcl_ext_eigen"] = "CONAN_PKG::eigen"
+        cmake.definitions["vcl_ext_fmt"] = "CONAN_PKG::fmt"
+        cmake.definitions["vcl_ext_glew"] = "CONAN_PKG::glew"
+
         return cmake
 
     def build(self):
         cmake = self._configure_cmake()
-        cmake.build()
+        cmake.configure(build_folder=self._build_subfolder)
+
+        if not hasattr(self.settings, "build_type"):
+            if cmake.is_multi_configuration:
+                cmake.build(target="vcl_core",     args=["--config","Debug"])
+                cmake.build(target="vcl_geometry", args=["--config","Debug"])
+                cmake.build(target="vcl_graphics", args=["--config","Debug"])
+                cmake.build(target="vcl_math",     args=["--config","Debug"])
+                cmake.build(target="vcl_core",     args=["--config","Release"])
+                cmake.build(target="vcl_geometry", args=["--config","Release"])
+                cmake.build(target="vcl_graphics", args=["--config","Release"])
+                cmake.build(target="vcl_math",     args=["--config","Release"])
+            else:
+                for config in ("Debug", "Release"):
+                    self.output.info("Building %s" % config)
+                    cmake.definitions["CMAKE_BUILD_TYPE"] = config
+                    cmake.configure(build_folder=self._build_subfolder)
+                    shutil.rmtree("CMakeFiles")
+                    os.remove("CMakeCache.txt")
+        else:
+            cmake.build(target="vcl_core")
+            cmake.build(target="vcl_geometry")
+            cmake.build(target="vcl_graphics")
+            cmake.build(target="vcl_math")
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        
+        bin_folder = os.path.join(self._build_subfolder, "bin")
+        lib_folder = os.path.join(self._build_subfolder, "lib")
+        inc_folder = os.path.join(self._source_subfolder, "src/libs")
+        self.copy("*.dll", dst="bin", src=bin_folder)
+        self.copy("*.a", dst="lib", src=lib_folder)
+        self.copy("*.lib", dst="lib", src=lib_folder)
+        self.copy("*.h", dst="include", src=inc_folder)
+        self.copy("*.inl", dst="include", src=inc_folder)
+        self.copy("config.h", dst="include/vcl.core/vcl/config", src=os.path.join(self._build_subfolder, self._source_subfolder, "src/libs/vcl.core/vcl/config"))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ['include/vcl.core', 'include/vcl.math', 'include/vcl.graphics', 'include/vcl.geometry']
+        if self.settings.os == "Windows":
+            if not hasattr(self.settings, "build_type"):
+                self.cpp_info.debug.libs = ['vcl_core_d.lib', 'vcl_math_d.lib', 'vcl_geometry_d.lib', 'vcl_graphics_d.lib']
+                self.cpp_info.release.libs = ['vcl_core.lib', 'vcl_math.lib', 'vcl_geometry.lib', 'vcl_graphics.lib']
+            else:
+                # Required for compatibility atm
+                if self.settings.build_type == 'Debug':
+                    self.cpp_info.libs = ['vcl_core_d.lib', 'vcl_math_d.lib', 'vcl_geometry_d.lib', 'vcl_graphics_d.lib']
+                else:
+                    self.cpp_info.libs = ['vcl_core.lib', 'vcl_math.lib', 'vcl_geometry.lib', 'vcl_graphics.lib']
+        else:
+            self.cpp_info.libs = ['libvcl_core.a', 'libvcl_math.a', 'libvcl_geometry.a', 'libvcl_graphics.a']
+        self.cpp_info.libdirs = [ "lib" ]
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,7 +1,46 @@
+// VCL configuration
+#include <vcl/config/global.h>
+
+// Include the relevant parts from the library
+#include <vcl/core/simd/vectorscalar.h>
+#include <vcl/math/math.h>
+
+// C++ standard library
 #include <iostream>
 
 int main()
 {
-    std::cout << "Bincrafters\n";
+	using Vcl::float4;
+	using Vcl::float8;
+	using Vcl::float16;
+
+	using Vcl::Mathematics::equal;
+
+	// Source data
+	float4  vec1{ 0.0f, 6.10116f, 11.6117f, 11.8436f };
+	float8  vec2{ 0.0f, 6.10116f, 11.6117f, 11.8436f,
+				  0.0f, 6.10116f, 11.6117f, 11.8436f };
+	float16 vec3{ 0.0f, 6.10116f, 11.6117f, 11.8436f,
+				  0.0f, 6.10116f, 11.6117f, 11.8436f,
+				  0.0f, 6.10116f, 11.6117f, 11.8436f,
+				  0.0f, 6.10116f, 11.6117f, 11.8436f };
+
+	// Compute 1 / sqrt(x)
+	float4  res1 = sqrt(vec1);
+	float8  res2 = sqrt(vec2);
+	float16 res3 = sqrt(vec3);
+
+	// Reference result
+	float4  ref1{ 0.0f, 2.4700526f, 3.4075945f, 3.4414532f };
+	float8  ref2{ 0.0f, 2.4700526f, 3.4075945f, 3.4414532f,
+				  0.0f, 2.4700526f, 3.4075945f, 3.4414532f };
+	float16 ref3{ 0.0f, 2.4700526f, 3.4075945f, 3.4414532f,
+				  0.0f, 2.4700526f, 3.4075945f, 3.4414532f,
+				  0.0f, 2.4700526f, 3.4075945f, 3.4414532f,
+				  0.0f, 2.4700526f, 3.4075945f, 3.4414532f };
+
+	std::cout << all(equal(ref1, res1, float4(1e-5f))) << std::endl;
+	std::cout << all(equal(ref2, res2, float8(1e-5f))) << std::endl;
+	std::cout << all(equal(ref3, res3, float16(1e-5f))) << std::endl;
     return 0;
 }
